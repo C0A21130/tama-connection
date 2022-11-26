2fe53a280eaad3af716f8cf0861006437a4c609f
C0A21130 <c0a2113061@edu.teu.ac.jp>
2022-11-27 01:39:37 +0900

    [fix] DB設定の修正

--- app/database.py
+++ app/database.py
@@ -1,13 +1,13 @@
-import os
 from pymongo import MongoClient
 
 # DBの設定
-DB_URL = os.environ["DB_URL"]
+DB_URL = "localhost"
+DB_PORT = 27017
 
 # DBに接続
 class DataBase:
     def __init__(self):
-        self.db = MongoClient(DB_URL)
+        self.db = MongoClient(DB_URL, DB_PORT)
         self.client = self.db["TamaConnection"]
 
     def get_collection(self, collection_name:str):
