e3865e311a6bcc68e403596791d4ef621ae4e504
C0A21130 <c0a2113061@edu.teu.ac.jp>
2022-11-27 23:37:16 +0900

    [update] 投稿記事の表示順序で最新が表示されるように変更

--- app/page.py
+++ app/page.py
@@ -51,12 +51,16 @@ class Page:
 
         # 結果を返却する
         if (tag == "kankou"):
+            kankou.reverse()
             return {"result":kankou[start_index : start_index+PAGES_NUM], "max":max_page}
         elif (tag == "gurume"):
+            gurume.reverse()
             return {"result":gurume[start_index : start_index+PAGES_NUM], "max":max_page}
         elif (tag == "tamasanpo"):
+            tamasanpo.reverse()
             return {"result":tamasanpo[start_index : start_index+PAGES_NUM], "max":max_page}
         else:
+            omiyage.reverse()
             return {"result":omiyage[start_index : start_index+PAGES_NUM], "max":max_page}
 
     # ページの投稿
